652df5ef4fc77e6c2099a66723c50092453cdfad
Ahsan_4 <ahsanjawed2001@gmail.com>
2022-02-27 15:38:50 +0500

    This is material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Grid,
+  Button,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+} from "@material-ui/core";
+import { PhotoCamera } from "@material-ui/icons";
+import useStyles from "./style";
 function App() {
+  const classes = useStyles();
+  const card = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <CssBaseline />
+      <AppBar>
+        <Toolbar>
+          <PhotoCamera className={classes.photoIcon} />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              color="textPrimary"
+              align="center"
+              gutterBottom
+            >
+              Album layout
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary">
+              Something short and leading about the collection below—its
+              contents, the creator, etc. Make it short and sweet, but not too
+              short so folks don't simply skip over it entirely.
+            </Typography>
+            <Grid
+              container
+              spacing={2}
+              justifyContent="center"
+              className={classes.buttonContainer}
+            >
+              <Grid item>
+                <Button variant="contained" color="primary">
+                  main call to action
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button variant="outlined" color="primary">
+                  secondary action
+                </Button>
+              </Grid>
+            </Grid>
+          </Container>
+        </div>
+      </main>
+      <main>
+        <div className={classes.cardDiv}>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container>
+            {card.map((x) => {
+              return(
+                <Grid
+                item
+                className={classes.grid}
+                xs={12}
+                sm={6}
+                md={4}
+                gutterBottom
+              >
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="card-image"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography variant="h5" gutterBottom>
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe
+                      the content.This is a media card. You can use this section to describe
+                      the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions className={classes.cardAction}>
+                    <Button size="small" color="primary">
+                      view
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+              )
+            })}
+          </Grid>
+        </Container>
+        </div>
+      </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center">
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textPrimary"
+          gutterBottom
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Something here to give the footer a purpose! <br />
+          Copyright © Your Website 2022.
+        </Typography>
+      </footer>
+    </>
   );
 }
-
 export default App;
---
+++ src/style.js
@@ -0,0 +1,36 @@
+import { makeStyles } from "@material-ui/styles";
+const useStyles = makeStyles((theme) => ({
+  photoIcon:{
+    marginRight:"10px"
+  },
+  container: {
+    backgroundColor:"#fff",
+    padding:"75px 0",
+    marginTop: "70px",
+  },
+  buttonContainer: {
+    marginTop: "30px",
+  },
+  cardGrid:{
+    padding:"75px 0",
+  },
+  grid:{
+    padding:"0 15px",
+    marginBottom:"10px"
+  },
+  card:{
+    height:"100%",
+   
+  },
+  cardMedia:{
+    paddingTop:"56.25%"
+  },
+  cardAction:{
+    marginBottom:"10px"
+  },
+  footer:{
+    backgroundColor:"#fff",
+    padding:"10px 0",
+  }
+}));
+export default useStyles;
